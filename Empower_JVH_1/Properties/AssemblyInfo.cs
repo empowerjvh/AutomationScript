c38f78e43977975cc1cd8f4f8bb94629b03fbf0d
Jan Vanhove <jan.vanhove@skyline.be>
2023-03-28 14:24:46 +0200

    Add project files.

---
+++ Empower_JVH_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_JVH_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_JVH_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D1F1E729-A26D-4129-ADCE-65C8CECA6242")]
+[assembly: AssemblyVersion("1.0.0.0")]
